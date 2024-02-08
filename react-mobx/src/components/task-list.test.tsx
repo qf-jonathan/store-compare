fa00756cfd28d70d9e7fc17a7e8a6f8ad4093111
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2024-02-08 15:14:07 -0500

    include task list test

--- react-mobx/src/components/task-list.test.tsx
+++ react-mobx/src/components/task-list.test.tsx
@@ -20,17 +20,19 @@ describe('<TaskList />', () => {
         },
       ];
     });
+    render(<TaskList />);
   });
 
-  it('render component', () => {
-    render(<TaskList />);
+  it('render component', async () => {
     expect(screen.getByPlaceholderText('Task description')).toBeVisible();
     expect(screen.getByRole('button', { name: 'Add Task' })).toBeVisible();
+    expect(screen.getByText('test description one')).toBeVisible();
+    expect(screen.getByText('test description two')).toBeVisible();
+    expect(screen.getAllByRole('button', { name: 'edit' }).length).toEqual(2);
+    expect(screen.getAllByRole('button', { name: 'remove' }).length).toEqual(2);
   });
 
   it('should add task', async () => {
-    render(<TaskList />);
-
     await userEvent.type(
       screen.getByPlaceholderText('Task description'),
       'test task description{enter}other example',
@@ -39,5 +41,26 @@ describe('<TaskList />', () => {
 
     expect(await screen.findByText('test task description')).toBeVisible();
     expect(await screen.findByText('other example')).toBeVisible();
+  });
+
+  it('should remove task', async () => {
+    await userEvent.click(screen.getAllByRole('button', {name: 'remove'})[0]);
+    expect(screen.queryByText('test description one')).not.toBeInTheDocument();
+    expect(screen.queryByText('test description two')).toBeVisible();
+
+    await userEvent.click(screen.getAllByRole('button', {name: 'remove'})[0]);
+    expect(screen.queryByText('test description two')).not.toBeInTheDocument();
+  });
+
+  it('should edit task', async () => {
+    userEvent.click(screen.getAllByRole('button', { name: 'edit' })[1]);
+    userEvent.type(
+      await screen.findByDisplayValue('test description two'),
+      '{backspace}{backspace}{backspace}other{enter}',
+    );
+
+    expect(
+      await screen.findByText('test description other'),
+    ).toBeInTheDocument();
   });
 });
